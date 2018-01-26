4c29495fbc0bc17f3adf24a4157f1b1a2e3954ad
Chakkra CHAK <chakkra.chak@canaltp.fr>
2018-01-26 16:35:35 +0100

    Start

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,30 +1,35 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { ErrorHandler, NgModule } from '@angular/core';
-import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
-import { SplashScreen } from '@ionic-native/splash-screen';
-import { StatusBar } from '@ionic-native/status-bar';
+import {BrowserModule} from '@angular/platform-browser';
+import {ErrorHandler, NgModule} from '@angular/core';
+import {IonicApp, IonicErrorHandler, IonicModule} from 'ionic-angular';
+import {SplashScreen} from '@ionic-native/splash-screen';
+import {StatusBar} from '@ionic-native/status-bar';
+import {Geolocation} from '@ionic-native/geolocation';
+import {GoogleMaps} from '@ionic-native/google-maps';
 
-import { MyApp } from './app.component';
-import { HomePage } from '../pages/home/home';
+import {MyApp} from './app.component';
+import {HomePage} from '../pages/home/home';
 
 @NgModule({
-  declarations: [
-    MyApp,
-    HomePage
-  ],
-  imports: [
-    BrowserModule,
-    IonicModule.forRoot(MyApp)
-  ],
-  bootstrap: [IonicApp],
-  entryComponents: [
-    MyApp,
-    HomePage
-  ],
-  providers: [
-    StatusBar,
-    SplashScreen,
-    {provide: ErrorHandler, useClass: IonicErrorHandler}
-  ]
+    declarations: [
+        MyApp,
+        HomePage
+    ],
+    imports: [
+        BrowserModule,
+        IonicModule.forRoot(MyApp)
+    ],
+    bootstrap: [IonicApp],
+    entryComponents: [
+        MyApp,
+        HomePage
+    ],
+    providers: [
+        StatusBar,
+        SplashScreen,
+        Geolocation,
+        GoogleMaps,
+        {provide: ErrorHandler, useClass: IonicErrorHandler}
+    ]
 })
-export class AppModule {}
+export class AppModule {
+}
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,47 @@
-import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import {Component, NgZone} from '@angular/core';
+import {NavController, Platform} from 'ionic-angular';
+import {Geolocation} from "@ionic-native/geolocation";
 
 @Component({
-  selector: 'page-home',
-  templateUrl: 'home.html'
+    selector: 'page-home',
+    templateUrl: 'home.html'
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+    currentAddress: {
+        label: string;
+        coords: {
+            lat: number;
+            lon: number;
+        }
+    } = {
+        label: "Waiting for location",
+        coords: undefined
+    };
 
-  }
+    constructor(public navCtrl: NavController, private platform: Platform, private zone: NgZone, private geolocation: Geolocation) {
+        this.platform.ready().then(() => {
+            this.getGeolocation();
+        })
+    }
 
+    getGeolocation() {
+        this.geolocation.getCurrentPosition({
+            timeout: 300000,
+            enableHighAccuracy: true,
+            maximumAge: 3600
+        }).then((resp) => {
+            this.zone.run(() => {
+                this.currentAddress = {
+                    label: resp.coords.latitude + ' ' + resp.coords.longitude,
+                    coords: {
+                        lat: resp.coords.latitude,
+                        lon: resp.coords.longitude
+                    }
+                }
+            });
+        }).catch((error) => {
+            alert('Error getting location : ' + JSON.stringify(error));
+        });
+    }
 }
